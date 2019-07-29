f869efbbcc5aa8e08a4965899cb00e86f1e0582c
eglobo911 <eglobo911@gmail.com>
2019-07-29 12:12:48 -0500

    Agregamos el README

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 // Express hbs engine
 hbs.registerPartials(__dirname + '/views/parciales');
